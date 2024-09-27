237af7603888345a2a4f88164ee3f14ab77928da
Pat Johnson <>
2024-09-27 13:41:37 -0400

    Updating config for deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ export default defineConfig({
       noExternal: ['swiper', 'leaflet']
     }
   },
-  site: 'https://hellotham.github.io',
+  site: 'https://tech-by-design.github.io',
   base: '/',
   integrations: [icon(), tailwind(), sitemap(), mdx(), alpinejs(), robotsTxt(), markdoc()],
   markdown: {
